f0619606213f706702e9562336206276c85e4019
Logan Gosch <logan.gosch.temp@gmail.com>
2019-09-11 09:50:39 -0500

    Gamer

---
+++ src/assignment/CelsiusToFahrenheit.java
@@ -0,0 +1,32 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class CelsiusToFahrenheit {
+    public static void main(String[] args)
+    {
+
+        double temperature;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the temperature for 8:00 in Celsius for conversion >>>");
+        temperature = input.nextDouble();
+        displayConversion(temperature, "8:00 AM ");
+        System.out.println("Enter the temperature for 12:00 in Celsius for conversion >>>");
+        temperature = input.nextDouble();
+        displayConversion(temperature, "12:00 PM ");
+        System.out.println("Enter the temperature for 5:00 in Celsius for conversion >>>");
+        temperature = input.nextDouble();
+        displayConversion(temperature, "5:00 PM ");
+
+    }
+    public static void displayConversion(double temperature, String time)
+    {
+        double tempConversion;
+        final double CELSIUS_TO_FAHRENHEIT = (temperature - 32) * .55;
+
+        tempConversion = temperature - CELSIUS_TO_FAHRENHEIT;
+        System.out.print("In " + temperature + " Celsius, it is " + tempConversion + " in Fahrenheit at " + time);
+    }
+
+}
--- src/assignment/MetricConversion.java
+++ src/assignment/MetricConversion.java
@@ -9,11 +9,15 @@ public class MetricConversion {
 
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.print("Enter a number for metric conversion");
+        System.out.print("Enter a number for metric conversion >>>");
         number = keyboard.nextInt();
 
+        displayGallonConversion(number);
+        displayInchConversion(number);
+
+
     }
-    public static void metricInchConversion(int number)
+    public static void displayInchConversion(int number)
     {
         final double  CENTIMETER_IN_INCH = 2.54;
         double numberConversion;
@@ -23,8 +27,13 @@ public class MetricConversion {
 
 
     }
-    public static void metricGallonConversion(int number)
+    public static void displayGallonConversion(int number)
     {
-        
+        final double LITERS_IN_GALLONS = 3.7854;
+        double numberConversion;
+
+        numberConversion = number * LITERS_IN_GALLONS;
+        System.out.print(" In " + number + " Gallons, there are " + numberConversion + " Liters");
+
     }
 }
